51f16c3c5357d19122c96d625abcf8982d50c6e4
DarkArush <144354468+DarkArush@users.noreply.github.com>
2023-09-24 19:45:48 +0530

    Update Area.py
    .......................................

--- Area.py
+++ Area.py
@@ -5,16 +5,16 @@ x = input("""Here is a list of some 2D shapes:1. Rectangle
 Select the Number of the Shape you want to calculate the area of: """)
 
 if x == "1":
-    l = int(input("Enter Length: "))
-    b = int(input("Enter Breadth: "))
-    ar_rec = l*b
+    l = float(input("Enter Length: "))
+    b = float(input("Enter Breadth: "))
+    ar_rec = l * b
     print("The Area is:", ar_rec, "cm.")
     u = input("""Do you want your answer to be in metres? 
                  Type 1. For Yes
                  Type 2. For No
                  Type your answer here: """)
     if u == "1":
-        m = ar_rec*100
+        m = ar_rec / 100
         print("Ok Then! The Answer is", m, "m.")
     elif u == "2":
         print("Ok Then! Your Final Answer is", ar_rec, "cm.")
@@ -22,15 +22,15 @@ if x == "1":
         print("Sorry! We can't give you the answer in this unit:", u, ".")
 
 elif x == "2":
-    s = int(input("Enter Length of the Side: "))
-    ar_sq = s*s
+    s = float(input("Enter Length of the Side: "))
+    ar_sq = s * s
     print("The Area is:", ar_sq, "cm.")
     u = input("""Do you want your answer to be in metres? 
                  Type 1. For Yes
                  Type 2. For No
                  Type your answer here: """)
     if u == "1":
-        m = ar_sq*100
+        m = ar_sq / 100
         print("Ok Then! The Answer is", m, "m.")
     elif u == "2":
         print("Ok Then! Your Final Answer is", ar_sq, "cm.")
@@ -38,16 +38,16 @@ elif x == "2":
         print("Sorry! We can't give you the answer in this unit:", u, ".")
 
 elif x == "3":
-    b = int(input("Enter Length of the Base: "))
-    h = int(input("Enter Height: "))
-    ar_triangle = 0.5*b*h
+    b = float(input("Enter Length of the Base: "))
+    h = float(input("Enter Height: "))
+    ar_triangle = 0.5 * b * h
     print("The Area is:", ar_triangle, "cm.")
     u = input("""Do you want your answer to be in metres? 
                  Type 1. For Yes
                  Type 2. For No
                  Type your answer here: """)
     if u == "1":
-        m = ar_triangle*100
+        m = ar_triangle / 100
         print("Ok Then! The Answer is", m, "m.")
     elif u == "2":
         print("Ok Then! Your Final Answer is", ar_triangle, "cm.")
@@ -55,16 +55,16 @@ elif x == "3":
         print("Sorry! We can't give you the answer in this unit:", u, ".")
 
 elif x == "4":
-    r = int(input("Enter Radius: "))
-    pie = 22/7
-    ar_circle = pie*r*r
+    r = float(input("Enter Radius: "))
+    pie = 22 / 7
+    ar_circle = pie * r * r
     print("The Area is:", ar_circle, "cm.")
     u = input("""Do you want your answer to be in metres? 
                  Type 1. For Yes
                  Type 2. For No
                  Type your answer here: """)
     if u == "1":
-        m = ar_circle*100
+        m = ar_circle / 100
         print("Ok Then! The Answer is", m, "m.")
     elif u == "2":
         print("Ok Then! Your Final Answer is", ar_circle, "cm.")
