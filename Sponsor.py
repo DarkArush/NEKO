a5487a8cbf81c44658e612ce1b8766503adb9b02
DarkArush <144354468+DarkArush@users.noreply.github.com>
2023-09-30 16:34:02 +0530

    Add files via upload
    ..........................................

---
+++ Sponsor.py
@@ -0,0 +1,14 @@
+learn = input("Do you want to learn Python? ")
+
+if learn == "yes":
+    print("Go to this Website: github.com/DarkArush")
+    print("It has many codes.You can Analyze it and learn from it.")
+
+elif learn == "no":
+    print("How Dare you!")
+    print("Even if you say no, I will take you there!")
+    print("Go to this Website: github.com/DarkArush")
+    print("It has many codes. Analyze it and learn from it.")
+
+else:
+    print("I can't understand a Word you are Saying.")
